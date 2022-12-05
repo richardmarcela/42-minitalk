655f7220b0e71ae030dfcc019ae00a0a1694a8cf
Marcela Richard De Araujo <mrichard@c1r15s13.42porto.com>
2022-12-05 22:09:16 +0000

    start

--- src/client.c
+++ src/client.c
@@ -3,46 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcela <marcela@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mrichard <mrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 10:43:59 by marcela           #+#    #+#             */
-/*   Updated: 2022/12/05 12:15:51 by marcela          ###   ########.fr       */
+/*   Updated: 2022/12/05 21:47:45 by mrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* 1 - create TCP socket
-2 - connect to the server
-3 - communicate with each other
-4 - close the connection */
 # include "minitalk.h"
 
 /* testo se o numero de parametros está correto e se o número do pid foi 
 passado apenas com números */
-int	ft_errors_in_main(int argc, char **argv)
+void	ft_errors_in_main(int argc, char **argv)
 {
 	int	i;
 
 	if (argc != 3)
-		return (1);
+		ft_printf("Wrong number of params!");
 	i = -1;
 	while (argv[1][++i])
 	{
 		if (!ft_strchr("0123456789", argv[1][i]))
-			return (1);
+		ft_printf("Incorrect PID!");
 	}
-	return (0);
 }
 
 void	handler_signal(int sig)
 {
-	
+	if (sig == SIGUSR1)
+		ft_printf("Signal %d coming out!\n", sig);
 }
 
 int	main(int argc, char **argv)
 {
-	if(ft_errors_in_main(argv, argv))
-		return (-1);
+	ft_errors_in_main(argc, argv);
 	ft_printf("\nClient PID: %d\n", getpid());
-	signal(SIGUSR1, handler_signal);
-	signal(SIGUSR2, handler_signal);
+	
 }
--- src/server.c
+++ src/server.c
@@ -3,28 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcela <marcela@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mrichard <mrichard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 10:43:57 by marcela           #+#    #+#             */
-/*   Updated: 2022/12/05 12:14:48 by marcela          ###   ########.fr       */
+/*   Updated: 2022/12/05 19:27:41 by mrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* 1 - create TCP socket 
-2 - bind the TCP socket to the server address (ip and port)
-3 - waiting for the clients
-4 - connection is estabilished between the clien and server 
-5 - communicate with each other
-6 - close the connection from the client
-
-SIGUSR1 e SIGUSR2 -> sinais reservados para o utilizador
- */
-# include "minitalk.h"
+/* SIGUSR1 e SIGUSR2 -> sinais reservados para o utilizador */
+#include "minitalk.h"
 
 int	main(void)
 {
-	char	server_message[2000], client_message[2000];
-
 	ft_printf("\nServer PID: %d\n", getpid());
-
 }
